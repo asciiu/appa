7c37ecf9a8fea2696735a309799e5fe7517e1787
Flowmaster <ellyssin.gimhae@gmail.com>
2018-12-14 17:48:09 -0700

    Added find order test.

--- order-service/db/sql/orders_test.go
+++ order-service/db/sql/orders_test.go
@@ -58,3 +58,41 @@ func TestInsertOrder(t *testing.T) {
 
 	repoUser.DeleteUserHard(db, user.ID)
 }
+
+func TestFindOrder(t *testing.T) {
+	db, err := db.NewDB("postgres://postgres@localhost/oldiez_test?&sslmode=disable")
+	checkErr(err)
+	defer db.Close()
+
+	user := models.NewUser("first", "last", "test@email", "hash")
+	_, error := repoUser.InsertUser(db, user)
+	if error != nil {
+		t.Errorf("%s", error)
+	}
+
+	now := string(pq.FormatTimestamp(time.Now().UTC()))
+	newOrder := protoOrder.Order{
+		OrderID:    uuid.New().String(),
+		UserID:     user.ID,
+		MarketName: "test-btc",
+		Side:       constants.Buy,
+		Size:       1.0,
+		Type:       constants.LimitOrder,
+		Status:     constants.Pending,
+		CreatedOn:  now,
+		UpdatedOn:  now,
+	}
+
+	repoOrder.InsertOrder(db, &newOrder)
+	findOrder, _ := repoOrder.FindOrder(db, newOrder.OrderID)
+
+	assert.Equal(t, newOrder.OrderID, findOrder.OrderID, "order ids not equal")
+	assert.Equal(t, newOrder.UserID, findOrder.UserID, "user ids not equal")
+	assert.Equal(t, newOrder.MarketName, findOrder.MarketName, "market names not equal")
+	assert.Equal(t, newOrder.Side, findOrder.Side, "sides not equal")
+	assert.Equal(t, newOrder.Size, findOrder.Size, "size not equal")
+	assert.Equal(t, newOrder.Type, findOrder.Type, "type not equal")
+	assert.Equal(t, newOrder.Status, findOrder.Status, "status not equal")
+
+	repoUser.DeleteUserHard(db, user.ID)
+}
