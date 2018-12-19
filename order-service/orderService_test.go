90faf0b156035707f0518cb497a1605d6ed0b6c7
Flowmaster <ellyssin.gimhae@gmail.com>
2018-12-18 19:26:35 -0700

    Added TestCancelOrderWrongUserID - a user cannot cancel another users order.

--- order-service/orderService_test.go
+++ order-service/orderService_test.go
@@ -152,3 +152,43 @@ func TestCancelOrder(t *testing.T) {
 
 	repoUser.DeleteUserHard(service.DB, user.ID)
 }
+
+func TestCancelOrderWrongUserID(t *testing.T) {
+	service, user := setupService()
+
+	defer service.DB.Close()
+
+	req := protoOrder.NewOrderRequest{
+		UserID:     user.ID,
+		MarketName: "test-btc",
+		Side:       constOrder.Sell,
+		Size:       1.0,
+		Type:       constOrder.LimitOrder,
+	}
+
+	res := protoOrder.OrderResponse{}
+	service.AddOrder(context.Background(), &req, &res)
+	assert.Equal(t, "success", res.Status, "expected success got: "+res.Message)
+	order := res.Data.Order
+
+	req2 := protoOrder.OrderRequest{
+		OrderID: order.OrderID,
+		UserID:  "a1c0e0dd-0c73-4b5e-ac5b-a2ac8378427d",
+	}
+
+	res2 := protoOrder.StatusResponse{}
+	service.CancelOrder(context.Background(), &req2, &res2)
+	assert.Equal(t, "success", res2.Status, "expected success got: "+res.Message)
+
+	req3 := protoOrder.OrderRequest{
+		OrderID: order.OrderID,
+		UserID:  user.ID,
+	}
+
+	res3 := protoOrder.OrderResponse{}
+	service.FindOrder(context.Background(), &req3, &res3)
+	assert.Equal(t, "success", res3.Status, "expected success got: "+res.Message)
+	assert.Equal(t, order.OrderID, res3.Data.Order.OrderID, "order IDs do not match")
+
+	repoUser.DeleteUserHard(service.DB, user.ID)
+}
