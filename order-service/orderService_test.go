74c789edacde60ced7f3fd032ddfeec7bbeb44b7
Flowmaster <ellyssin.gimhae@gmail.com>
2018-12-18 19:23:20 -0700

    Added TestCancelOrder.

--- order-service/orderService_test.go
+++ order-service/orderService_test.go
@@ -109,7 +109,46 @@ func TestFindOrderWrongUserID(t *testing.T) {
 
 	res2 := protoOrder.OrderResponse{}
 	service.FindOrder(context.Background(), &req2, &res2)
-	assert.Equal(t, "nonentity", res2.Status, "expected success got: "+res.Message)
+	assert.Equal(t, "nonentity", res2.Status, "expected nonentity got: "+res.Message)
+
+	repoUser.DeleteUserHard(service.DB, user.ID)
+}
+
+func TestCancelOrder(t *testing.T) {
+	service, user := setupService()
+
+	defer service.DB.Close()
+
+	req := protoOrder.NewOrderRequest{
+		UserID:     user.ID,
+		MarketName: "test-btc",
+		Side:       constOrder.Sell,
+		Size:       1.0,
+		Type:       constOrder.LimitOrder,
+	}
+
+	res := protoOrder.OrderResponse{}
+	service.AddOrder(context.Background(), &req, &res)
+	assert.Equal(t, "success", res.Status, "expected success got: "+res.Message)
+	order := res.Data.Order
+
+	req2 := protoOrder.OrderRequest{
+		OrderID: order.OrderID,
+		UserID:  user.ID,
+	}
+
+	res2 := protoOrder.StatusResponse{}
+	service.CancelOrder(context.Background(), &req2, &res2)
+	assert.Equal(t, "success", res2.Status, "expected success got: "+res.Message)
+
+	req3 := protoOrder.OrderRequest{
+		OrderID: order.OrderID,
+		UserID:  user.ID,
+	}
+
+	res3 := protoOrder.OrderResponse{}
+	service.FindOrder(context.Background(), &req3, &res3)
+	assert.Equal(t, "nonentity", res3.Status, "expected nonentity got: "+res.Message)
 
 	repoUser.DeleteUserHard(service.DB, user.ID)
 }
