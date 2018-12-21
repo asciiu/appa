1b96198a5f8ea9ae9a3ab20111757a185d1c6d76
Flowmaster <ellyssin.gimhae@gmail.com>
2018-12-20 23:11:28 -0700

    Added order book test.

---
+++ order-service/models/orderBook_test.go
@@ -0,0 +1,27 @@
+package models
+
+import (
+	"testing"
+
+	constOrder "github.com/asciiu/appa/order-service/constants"
+	protoOrder "github.com/asciiu/appa/order-service/proto/order"
+	"github.com/google/uuid"
+	"github.com/stretchr/testify/assert"
+)
+
+func TestOrderBook(t *testing.T) {
+	book := NewOrderBook("test-btc")
+	order := protoOrder.Order{
+		OrderID:    uuid.New().String(),
+		UserID:     uuid.New().String(),
+		MarketName: "test-btc",
+		Side:       constOrder.Buy,
+		Size:       1,
+		Price:      0.01,
+		Type:       constOrder.LimitOrder,
+	}
+	book.AddOrder(&order)
+
+	assert.Equal(t, 1, len(book.BuyQ), "should be 1 order in buys")
+	assert.Equal(t, 0, len(book.SellQ), "should be 0 order in sells")
+}
