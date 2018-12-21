86a898f0e660872c356b5241d0ba6d2fa5614c39
Flowmaster <ellyssin.gimhae@gmail.com>
2018-12-20 23:16:57 -0700

    Added TestOrderBookWrongOrder - order books should not accept orders from differing markets.

--- order-service/models/orderBook_test.go
+++ order-service/models/orderBook_test.go
@@ -25,3 +25,20 @@ func TestOrderBook(t *testing.T) {
 	assert.Equal(t, 1, len(book.BuyQ), "should be 1 order in buys")
 	assert.Equal(t, 0, len(book.SellQ), "should be 0 order in sells")
 }
+
+func TestOrderBookWrongOrder(t *testing.T) {
+	book := NewOrderBook("test-btc")
+	order := protoOrder.Order{
+		OrderID:    uuid.New().String(),
+		UserID:     uuid.New().String(),
+		MarketName: "bch-btc",
+		Side:       constOrder.Buy,
+		Size:       1,
+		Price:      0.01,
+		Type:       constOrder.LimitOrder,
+	}
+	book.AddOrder(&order)
+
+	assert.Equal(t, 0, len(book.BuyQ), "should be 0 order in buys")
+	assert.Equal(t, 0, len(book.SellQ), "should be 0 order in sells")
+}
