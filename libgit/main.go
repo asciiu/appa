715c3c5a4cc349185e5c1688b72ddf5a7e740af4
Flowmaster <ellyssin.gimhae@gmail.com>
2019-04-30 22:28:05 -0600

    Init new repo example.

--- libgit/main.go
+++ libgit/main.go
@@ -1,12 +1,17 @@
 package main
 
 import (
+	"fmt"
+
 	"gopkg.in/libgit2/git2go.v27"
 )
 
 func main() {
-	_, err := git.Clone("git://github.com/gopheracademy/gopheracademy-web.git", "web", &git.CloneOptions{})
+	// init a new git repo under the web directory
+	repo, err := git.InitRepository("web", false)
+	//repo, err := git.Clone("git://github.com/gopheracademy/gopheracademy-web.git", "web", &git.CloneOptions{})
 	if err != nil {
 		panic(err)
 	}
+	fmt.Println(repo)
 }
