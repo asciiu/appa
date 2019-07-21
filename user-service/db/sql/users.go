7e13021111ad94173a43c9f7b7eba9c446210ab4
Flowmaster <ellyssin.gimhae@gmail.com>
2019-07-21 13:29:59 -0600

    Find user by email, soft delete of user, and update user password with tests.

--- user-service/db/sql/users.go
+++ user-service/db/sql/users.go
@@ -12,34 +12,37 @@ func DeleteUserHard(db *sql.DB, userID string) error {
 	return err
 }
 
-// func DeleteUserSoft(db *sql.DB, userID string) error {
-// 	_, err := db.Exec("UPDATE users SET deleted_on = now() WHERE id = $1", userID)
-// 	return err
-// }
+// DeleteUserSoft - sets the deleated_on column
+func DeleteUserSoft(db *sql.DB, userID string) error {
+	_, err := db.Exec("UPDATE users SET deleted_on = now() WHERE id = $1", userID)
+	return err
+}
 
-// func FindUser(db *sql.DB, email string) (*models.User, error) {
-// 	var u models.User
-// 	err := db.QueryRow("SELECT
-// 	    id,
-// 	    username,
-// 	    email,
-// 	    email_verified,
-// 	    password_hash,
-// 	    created_on,
-// 	    updated_on
-// 	 FROM users WHERE email = $1", email).
-// 		Scan(&u.ID,
-// 			&u.First,
-// 			&u.Last,
-// 			&u.Email,
-// 			&u.EmailVerified,
-// 			&u.PasswordHash)
+// FindUserByEmail - find a user by email address
+func FindUserByEmail(db *sql.DB, email string) (*models.User, error) {
+	var u models.User
+	err := db.QueryRow(`SELECT
+	    id,
+	    username,
+	    email,
+	    email_verified,
+	    password_hash,
+	    created_on,
+	    updated_on
+	    FROM users WHERE email = $1`, email).
+		Scan(&u.ID,
+			&u.Username,
+			&u.Email,
+			&u.EmailVerified,
+			&u.PasswordHash,
+			&u.CreatedOn,
+			&u.UpdatedOn)
 
-// 	if err != nil {
-// 		return nil, err
-// 	}
-// 	return &u, nil
-// }
+	if err != nil {
+		return nil, err
+	}
+	return &u, nil
+}
 
 // FindUserByID - find users by the user id
 func FindUserByID(db *sql.DB, userID string) (*models.User, error) {
@@ -51,7 +54,8 @@ func FindUserByID(db *sql.DB, userID string) (*models.User, error) {
 	    email_verified, 
 	    password_hash,
 	    created_on,
-	    updated_on FROM users WHERE id = $1`, userID).
+		updated_on 
+		FROM users WHERE id = $1`, userID).
 		Scan(&u.ID,
 			&u.Username,
 			&u.Email,
@@ -92,10 +96,11 @@ func InsertUser(db *sql.DB, user *models.User) (*models.User, error) {
 	return user, nil
 }
 
-// func UpdateUserPassword(db *sql.DB, userID, hash string) error {
-// 	_, err := db.Exec("UPDATE users SET password_hash = $1 WHERE id = $2", hash, userID)
-// 	return err
-// }
+// UpdateUserPassword - update a user's password
+func UpdateUserPassword(db *sql.DB, userID, hash string) error {
+	_, err := db.Exec("UPDATE users SET password_hash = $1 WHERE id = $2", hash, userID)
+	return err
+}
 
 // func UpdateUserInfo(db *sql.DB, user *models.User) (*models.User, error) {
 // 	sqlStatement := `UPDATE users SET first_name = $1, last_name = $2, email = $3 WHERE id = $4`
--- user-service/db/sql/users_test.go
+++ user-service/db/sql/users_test.go
@@ -27,9 +27,6 @@ func TestInsertUser(t *testing.T) {
 	newUser, err := sql.InsertUser(db, user)
 	assert.Nil(t, err, fmt.Sprintf("could not insert user %s", err))
 
-	if err != nil {
-		t.Errorf("%s", err)
-	}
 	foundUser, err := sql.FindUserByID(db, user.ID)
 	assert.Nil(t, err, fmt.Sprintf("could not find user by id %s", err))
 
@@ -41,20 +38,50 @@ func TestInsertUser(t *testing.T) {
 	assert.Nil(t, err, fmt.Sprintf("could not delete user %s", err))
 }
 
-// func TestFindUser(t *testing.T) {
-// 	db, err := db.NewDB("postgres://postgres@localhost/appa_test?&sslmode=disable")
-// 	checkErr(err)
-// 	defer db.Close()
+func TestFindUserByEmail(t *testing.T) {
+	db, err := db.NewDB("postgres://postgres@localhost/appa_test?&sslmode=disable")
+	checkErr(err)
+	defer db.Close()
 
-// 	email := "test@email"
-// 	user, err := sql.FindUser(db, email)
-// 	if err != nil {
-// 		t.Errorf("%s", err)
-// 	}
-// 	if user == nil {
-// 		t.Errorf("user not found %s", email)
-// 	}
+	email := "testing@email"
+	user := models.NewUser("jonni6", email, "password")
+	newUser, err := sql.InsertUser(db, user)
+	assert.Nil(t, err, fmt.Sprintf("could not insert user %s", err))
 
-// 	sqlStatement := `delete from users where email = $1`
-// 	db.Exec(sqlStatement, email)
-// }
+	foundUser, err := sql.FindUserByEmail(db, email)
+	assert.Nil(t, err, fmt.Sprintf("could not find user by email %s", err))
+
+	assert.Equal(t, newUser.ID, foundUser.ID, "user ids do not match")
+	assert.Equal(t, newUser.Username, foundUser.Username, "usernames do not match")
+	assert.Equal(t, newUser.Email, foundUser.Email, "emails do not match")
+
+	err = sql.DeleteUserHard(db, user.ID)
+	assert.Nil(t, err, fmt.Sprintf("could not delete user %s", err))
+}
+
+func TestUpdateUserPassword(t *testing.T) {
+	db, err := db.NewDB("postgres://postgres@localhost/appa_test?&sslmode=disable")
+	checkErr(err)
+	defer db.Close()
+
+	email := "testing@email"
+	user := models.NewUser("jonni6", email, "password")
+	newUser, err := sql.InsertUser(db, user)
+	assert.Nil(t, err, fmt.Sprintf("could not insert user %s", err))
+
+	newHash := models.HashAndSalt([]byte("newpwd"))
+
+	err = sql.UpdateUserPassword(db, user.ID, newHash)
+	assert.Nil(t, err, fmt.Sprintf("could not update user password %s", err))
+
+	foundUser, err := sql.FindUserByEmail(db, email)
+	assert.Nil(t, err, fmt.Sprintf("could not find user by email %s", err))
+
+	assert.Equal(t, newUser.ID, foundUser.ID, "user ids do not match")
+	assert.Equal(t, newUser.Username, foundUser.Username, "usernames do not match")
+	assert.Equal(t, newUser.Email, foundUser.Email, "emails do not match")
+	assert.Equal(t, newHash, foundUser.PasswordHash, "password hash does not match")
+
+	err = sql.DeleteUserHard(db, user.ID)
+	assert.Nil(t, err, fmt.Sprintf("could not delete user %s", err))
+}
