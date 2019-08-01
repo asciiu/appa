9542cc6c1c15166cb741cb61005a2a0adcf9b42f
Flowmaster <ellyssin.gimhae@gmail.com>
2019-07-31 19:46:54 -0600

    The final story image should have an alpine os with libgit2-0.27 installed.

--- story-service/README.md
+++ story-service/README.md
@@ -5,6 +5,8 @@
 - git2go v27
 
 ### References
+- https://micro.mu/docs/deploy-docker.html
+- https://micro.mu/docs/deploy-kubernetes.html#getting-started
 - https://libgit2.org
 - https://github.com/libgit2/libgit2
 - https://github.com/libgit2/git2go
--- story-service/main.go
+++ story-service/main.go
@@ -8,8 +8,7 @@ import (
 	"github.com/asciiu/appa/common/db"
 	protoStory "github.com/asciiu/appa/story-service/proto/story"
 	micro "github.com/micro/go-micro"
-	//k8s "github.com/micro/kubernetes/go/micro"
-	k8s "github.com/micro/examples/kubernetes/go/micro"
+	k8s "github.com/micro/kubernetes/go/micro"
 )
 
 func check(e error) {
@@ -18,37 +17,22 @@ func check(e error) {
 	}
 }
 
-func NewStoryService(name, dbUrl string) micro.Service {
-	// Create a new service. Include some options here.
+func main() {
+	dbURL := fmt.Sprintf("%s", os.Getenv("DB_URL"))
+
 	srv := k8s.NewService(
-		// This name must match the package name given in your protobuf definition
-		micro.Name(name),
+		micro.Name("stories"),
 		micro.Version("latest"),
 	)
-
-	// Init will parse the command line flags.
 	srv.Init()
 
-	appaDB, err := db.NewDB(dbUrl)
-
-	if err != nil {
-		log.Fatalf(err.Error())
-	}
+	appaDB, err := db.NewDB(dbURL)
+	check(err)
 
 	service := StoryService{
 		DB: appaDB,
 	}
-	// Register our service with the gRPC server, this will tie our
-	// implementation into the auto-generated interface code for our
-	// protobuf definition.
 	protoStory.RegisterStoryServiceHandler(srv.Server(), &service)
-
-	return srv
-}
-
-func main() {
-	dbUrl := fmt.Sprintf("%s", os.Getenv("DB_URL"))
-	srv := NewStoryService("stories", dbUrl)
 
 	if err := srv.Run(); err != nil {
 		log.Fatal(err)
