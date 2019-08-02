5e2bd53092b2d3c0a5078c5a2dd5683449d25793
Flowmaster <ellyssin.gimhae@gmail.com>
2019-08-02 15:20:59 -0600

    Refactor main.

--- socket/main.go
+++ socket/main.go
@@ -37,23 +37,58 @@ package main
 import (
 	"fmt"
 	"log"
+	"net/http"
 	"os"
 
 	"github.com/asciiu/appa/common/db"
+	"github.com/asciiu/appa/socket/controllers"
+	"github.com/asciiu/appa/socket/middlewares"
+	"github.com/labstack/echo"
 	_ "github.com/lib/pq"
+	micro "github.com/micro/go-micro"
+	k8s "github.com/micro/kubernetes/go/micro"
 )
+
+func health(c echo.Context) error {
+	return c.String(http.StatusOK, "")
+}
 
 func main() {
 	dbURL := fmt.Sprintf("%s", os.Getenv("DB_URL"))
-	appDB, err := db.NewDB(dbURL)
-	if err != nil {
+
+	if database, err := db.NewDB(dbURL); err != nil {
 		log.Printf("ERROR: %s", err)
 	} else {
-		defer appDB.Close()
-		router := NewRouter(appDB)
+		defer database.Close()
+
+		echo := echo.New()
+		//echo.AutoTLSManager.Prompt = autocert.AcceptTOS
+		//echo.AutoTLSManager.HostPolicy = autocert.HostWhitelist("stage.fomo.exchange")
+		//echo.AutoTLSManager.Cache = autocert.DirCache("/mnt/fomo/autocert")
+
+		middlewares.SetMainMiddlewares(echo)
+
+		// handle socket connections from socketController
+		socketController := controllers.NewWebsocketController()
+
+		// websocket ticker
+		echo.GET("/ws", socketController.Connect)
+
+		// required for health checks
+		echo.GET("/index.html", health)
+		echo.GET("/", health)
+
+		service := k8s.NewService(micro.Name("socket-service"))
+		service.Init()
+		go func() {
+			if err := service.Run(); err != nil {
+				log.Println("nope! ", err)
+			}
+		}()
+
 		// HTTPs server
-		//router.Logger.Fatal(router.StartAutoTLS(":443"))
+		//echo.Logger.Fatal(echo.StartAutoTLS(":443"))
 		port := fmt.Sprintf("%s", os.Getenv("SOCKET_PORT"))
-		router.Logger.Fatal(router.Start(":" + port))
+		echo.Logger.Fatal(echo.Start(":" + port))
 	}
 }
