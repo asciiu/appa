206c5f67b35563f163a28cf98a35fd8841c03ec7
Flowmaster <ellyssin.gimhae@gmail.com>
2020-03-10 20:47:34 -0600

    Test signup.

---
+++ api-graphql-rocket/server/mutation_test.go
@@ -0,0 +1,37 @@
+package server_test
+
+import (
+	"context"
+	"log"
+	"testing"
+
+	"github.com/asciiu/appa/api-graphql-rocket/server"
+	userQuery "github.com/asciiu/appa/lib/user/db/sql"
+	"github.com/stretchr/testify/assert"
+)
+
+func TestSignup(t *testing.T) {
+	cfg := server.Config{
+		RedisURL: "localhost:6379",
+		DBURL:    "postgres://postgres@localhost:5432/appa_test?sslmode=disable",
+	}
+
+	s, err := server.NewGraphQLServer(cfg)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	email := "test.email"
+	username := "jerry"
+
+	user, err := s.Signup(context.Background(), email, username, "password")
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	assert.Equal(t, email, user.Email, "email does not match")
+	assert.Equal(t, username, user.Username, "username does not match")
+	assert.Equal(t, false, user.EmailVerified, "email verified should be false")
+
+	userQuery.DeleteUserHard(s.DB, user.ID)
+}
