4b25ef51b8e2c8fcbd0a1aa5eda028741db9afce
Flowmaster <ellyssin.gimhae@gmail.com>
2020-03-13 15:00:12 -0600

    Cat recognizer.

--- micro-image-recognizer/go.mod
+++ micro-image-recognizer/go.mod
@@ -6,5 +6,6 @@ go 1.14
 
 require (
 	github.com/golang/protobuf v1.3.5 // indirect
+	github.com/julienschmidt/httprouter v1.3.0 // indirect
 	github.com/tensorflow/tensorflow v2.1.0+incompatible
 )
--- micro-image-recognizer/main.go
+++ micro-image-recognizer/main.go
@@ -1,28 +1,204 @@
 package main
 
 import (
-	"fmt"
+	"bufio"
+	"bytes"
+	"encoding/json"
+	"io"
+	"io/ioutil"
+	"log"
+	"net/http"
+	"os"
+	"sort"
+	"strings"
+
+	"github.com/julienschmidt/httprouter"
 	tf "github.com/tensorflow/tensorflow/tensorflow/go"
 	"github.com/tensorflow/tensorflow/tensorflow/go/op"
 )
 
-func main() {
-	// Construct a graph with an operation that produces a string constant.
-	s := op.NewScope()
-	c := op.Const(s, "Hello from TensorFlow version "+tf.Version())
-	graph, err := s.Finalize()
+var (
+	graph  *tf.Graph
+	labels []string
+)
+
+type ClassifyResult struct {
+	Filename string        `json:"filename"`
+	Labels   []LabelResult `json:"labels"`
+}
+
+type LabelResult struct {
+	Label       string  `json:"label"`
+	Probability float32 `json:"probability"`
+}
+
+type ByProbability []LabelResult
+
+func (a ByProbability) Len() int           { return len(a) }
+func (a ByProbability) Swap(i, j int)      { a[i], a[j] = a[j], a[i] }
+func (a ByProbability) Less(i, j int) bool { return a[i].Probability > a[j].Probability }
+
+func findBestLabels(probabilities []float32) []LabelResult {
+	// Make a list of label/probability pairs
+	var resultLabels []LabelResult
+	for i, p := range probabilities {
+		if i >= len(labels) {
+			break
+		}
+		resultLabels = append(resultLabels, LabelResult{Label: labels[i], Probability: p})
+	}
+	// Sort by probability
+	sort.Sort(ByProbability(resultLabels))
+	// Return top 5 labels
+	return resultLabels[:5]
+}
+
+func loadModel() error {
+	// Load inception model
+	model, err := ioutil.ReadFile("model/tensorflow_inception_graph.pb")
 	if err != nil {
-		panic(err)
+		return err
+	}
+	graph = tf.NewGraph()
+	if err := graph.Import(model, ""); err != nil {
+		return err
+	}
+	// Load labels
+	labelsFile, err := os.Open("model/imagenet_comp_graph_label_strings.txt")
+	if err != nil {
+		return err
+	}
+	defer labelsFile.Close()
+	scanner := bufio.NewScanner(labelsFile)
+	// Labels are separated by newlines
+	for scanner.Scan() {
+		labels = append(labels, scanner.Text())
+	}
+	if err := scanner.Err(); err != nil {
+		return err
+	}
+	return nil
+}
+
+func responseError(w http.ResponseWriter, message string, code int) {
+	w.Header().Set("Content-Type", "application/json")
+	w.WriteHeader(code)
+	json.NewEncoder(w).Encode(map[string]string{"error": message})
+}
+
+func responseJSON(w http.ResponseWriter, data interface{}) {
+	w.Header().Set("Content-Type", "application/json")
+	json.NewEncoder(w).Encode(data)
+}
+
+func makeTransformImageGraph(imageFormat string) (graph *tf.Graph, input, output tf.Output, err error) {
+	const (
+		H, W  = 224, 224
+		Mean  = float32(117)
+		Scale = float32(1)
+	)
+	s := op.NewScope()
+	input = op.Placeholder(s, tf.String)
+	// Decode PNG or JPEG
+	var decode tf.Output
+	if imageFormat == "png" {
+		decode = op.DecodePng(s, input, op.DecodePngChannels(3))
+	} else {
+		decode = op.DecodeJpeg(s, input, op.DecodeJpegChannels(3))
+	}
+	// Div and Sub perform (value-Mean)/Scale for each pixel
+	output = op.Div(s,
+		op.Sub(s,
+			// Resize to 224x224 with bilinear interpolation
+			op.ResizeBilinear(s,
+				// Create a batch containing a single image
+				op.ExpandDims(s,
+					// Use decoded pixel values
+					op.Cast(s, decode, tf.Float),
+					op.Const(s.SubScope("make_batch"), int32(0))),
+				op.Const(s.SubScope("size"), []int32{H, W})),
+			op.Const(s.SubScope("mean"), Mean)),
+		op.Const(s.SubScope("scale"), Scale))
+	graph, err = s.Finalize()
+	return graph, input, output, err
+}
+
+func makeTensorFromImage(imageBuffer *bytes.Buffer, imageFormat string) (*tf.Tensor, error) {
+	tensor, err := tf.NewTensor(imageBuffer.String())
+	if err != nil {
+		return nil, err
+	}
+	graph, input, output, err := makeTransformImageGraph(imageFormat)
+	if err != nil {
+		return nil, err
+	}
+	session, err := tf.NewSession(graph, nil)
+	if err != nil {
+		return nil, err
+	}
+	defer session.Close()
+	normalized, err := session.Run(
+		map[tf.Output]*tf.Tensor{input: tensor},
+		[]tf.Output{output},
+		nil)
+	if err != nil {
+		return nil, err
+	}
+	return normalized[0], nil
+}
+
+func recognizeHandler(w http.ResponseWriter, r *http.Request, _ httprouter.Params) {
+	// Read image
+	imageFile, header, err := r.FormFile("image")
+	// Will contain filename and extension
+	imageName := strings.Split(header.Filename, ".")
+	if err != nil {
+		responseError(w, "Could not read image", http.StatusBadRequest)
+		return
+	}
+	defer imageFile.Close()
+	var imageBuffer bytes.Buffer
+	// Copy image data to a buffer
+	io.Copy(&imageBuffer, imageFile)
+
+	tensor, err := makeTensorFromImage(&imageBuffer, imageName[:1][0])
+	if err != nil {
+		responseError(w, "Invalid image", http.StatusBadRequest)
+		return
 	}
 
-	// Execute the graph in a session.
-	sess, err := tf.NewSession(graph, nil)
+	session, err := tf.NewSession(graph, nil)
 	if err != nil {
-		panic(err)
+		log.Fatal(err)
 	}
-	output, err := sess.Run(nil, []tf.Output{c}, nil)
+	defer session.Close()
+	output, err := session.Run(
+		map[tf.Output]*tf.Tensor{
+			graph.Operation("input").Output(0): tensor,
+		},
+		[]tf.Output{
+			graph.Operation("output").Output(0),
+		},
+		nil)
 	if err != nil {
-		panic(err)
+		responseError(w, "Could not run inference", http.StatusInternalServerError)
+		return
 	}
-	fmt.Println(output[0].Value())
+
+	responseJSON(w, ClassifyResult{
+		Filename: header.Filename,
+		Labels:   findBestLabels(output[0].Value().([][]float32)[0]),
+	})
+	// ...
+}
+
+func main() {
+	if err := loadModel(); err != nil {
+		log.Fatal(err)
+		return
+	}
+
+	r := httprouter.New()
+	r.POST("/recognize", recognizeHandler)
+	log.Fatal(http.ListenAndServe(":8080", r))
 }
