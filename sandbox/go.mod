46a74d6276c0f84d4f4af154be9dfe296743e067
Flowmaster <ellyssin.gimhae@gmail.com>
2020-06-13 13:04:02 -0600

    Sandbox curry.

--- sandbox/go.mod
+++ sandbox/go.mod
@@ -1,12 +1,5 @@
-module github.com/asciiu/appa/youtube
+module github.com/asciiu/appa/sandbox
 
 go 1.14
 
 replace github.com/asciiu/appa/lib => ../lib
-
-require (
-	github.com/99designs/gqlgen v0.11.3
-	github.com/asciiu/appa/lib v0.0.0-00010101000000-000000000000 // indirect
-	github.com/kkdai/youtube v1.2.1 // indirect
-	github.com/vektah/gqlparser/v2 v2.0.1
-)
--- sandbox/main.go
+++ sandbox/main.go
@@ -1,23 +1,28 @@
 package main
 
-import (
-	"flag"
-	"fmt"
-	"log"
-	"os/user"
-	"path/filepath"
-)
+import "fmt"
+
+type Bet struct {
+	wagerAmount uint
+}
+
+func findUser(db string) func(string) string {
+	return func(userID string) string {
+		user := fmt.Sprintf("%s %s", db, userID)
+		return user
+	}
+}
+
+func WithStatus(db string) func(status string) string {
+	return func(status string) string {
+		str := fmt.Sprintf("%s %s", db, status)
+		return str
+	}
+}
 
 func main() {
-	flag.Parse()
-	log.Println(flag.Args())
+	find := findUser("database")
+	str := find("1234")
 
-	usr, _ := user.Current()
-	downloadDir := fmt.Sprintf("%v/Movies/youtubedr", usr.HomeDir)
-	log.Println("download to dir=", downloadDir)
-
-	// convert mp4
-	path := filepath.Join(downloadDir, "LWE79K2Ii-s.mp4")
-
-	fmt.Println(path)
+	fmt.Println(str)
 }
