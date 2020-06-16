98070276b1f5e0e7561eac3c8f626805d0095f2a
Flowmaster <ellyssin.gimhae@gmail.com>
2020-06-15 20:21:26 -0600

    Retrieve summary info from grin.

---
+++ sandbox/README.md
@@ -0,0 +1,11 @@
+# Resources
+
+* https://github.com/mimblewimble/docs/wiki/Wallet-JSON-RPC-API-Guide
+* https://docs.rs/grin_wallet_api/3.1.1/grin_wallet_api/#functions
+
+
+Running the node and wallet-api:
+```
+$ grin --floonet
+$ grin-wallet --floonet owner_api
+```
--- sandbox/main.go
+++ sandbox/main.go
@@ -1,9 +1,11 @@
 package main
 
 import (
+	"encoding/json"
 	"fmt"
 	"log"
 	"os"
+	"strconv"
 
 	"github.com/asciiu/appa/lib/config"
 	"github.com/kelseyhightower/envconfig"
@@ -17,11 +19,14 @@ func check(err error) {
 }
 
 type WalletInfo struct {
-	AmountAwaitingConfirmation int `json:"amount_awaiting_confirmation"`
-	AmountAwaitingFinalization int `json:"amount_awaiting_finalization"`
-	AmountCurrentSpendable     int `json:"amount_currently_spendable"`
-	LastConfirmedHeight        int `json:"last_confirmed_height"`
-	MinConfirmations           int `json:"minimum_confirmations"`
+	AmountAwaitingConfirmation int64 `json:"amount_awaiting_confirmation"`
+	AmountAwaitingFinalization int64 `json:"amount_awaiting_finalization"`
+	AmountCurrentSpendable     int64 `json:"amount_currently_spendable"`
+	AmountImmature             int64 `json:"amount_immature"`
+	AmountLocked               int64 `json:"amount_locked"`
+	LastConfirmedHeight        int64 `json:"last_confirmed_height"`
+	MinimumConfirmations       int64 `json:"minimum_confirmations"`
+	Total                      int64 `json:"total"`
 }
 
 type GrinConfig struct {
@@ -47,5 +52,34 @@ func main() {
 		log.Fatal(err)
 	}
 
-	fmt.Printf("%+v\n", response)
+	resMap := make(map[string]interface{}, 0)
+	response.GetObject(&resMap)
+
+	if val, ok := resMap["Ok"]; ok {
+		tup := val.([]interface{})
+		wMap := tup[1].(map[string]interface{})
+
+		amountAwaitingConfirmation, _ := strconv.ParseInt(wMap["amount_awaiting_confirmation"].(string), 10, 64)
+		amountAwaitingFinalization, _ := strconv.ParseInt(wMap["amount_awaiting_finalization"].(string), 10, 64)
+		amountCurrentSpendable, _ := strconv.ParseInt(wMap["amount_currently_spendable"].(string), 10, 64)
+		amountImmature, _ := strconv.ParseInt(wMap["amount_immature"].(string), 10, 64)
+		amountLocked, _ := strconv.ParseInt(wMap["amount_locked"].(string), 10, 64)
+		lastConfirmedHeight, _ := strconv.ParseInt(wMap["last_confirmed_height"].(string), 10, 64)
+		minimumConfirmations, _ := strconv.ParseInt(wMap["minimum_confirmations"].(string), 10, 64)
+		total, _ := strconv.ParseInt(wMap["total"].(string), 10, 64)
+
+		walletInfo := WalletInfo{
+			AmountAwaitingConfirmation: amountAwaitingConfirmation,
+			AmountAwaitingFinalization: amountAwaitingFinalization,
+			AmountCurrentSpendable:     amountCurrentSpendable,
+			AmountImmature:             amountImmature,
+			AmountLocked:               amountLocked,
+			LastConfirmedHeight:        lastConfirmedHeight,
+			MinimumConfirmations:       minimumConfirmations,
+			Total:                      total,
+		}
+
+		jsn, _ := json.Marshal(walletInfo)
+		fmt.Printf("%s\n", jsn)
+	}
 }
